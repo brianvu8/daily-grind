2974000cfe617ff9ca129de26266dc7f712b0cf5
Brian Vu <brianvu888@gmail.com>
2024-11-20 15:21:17 -0800

    dada

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,7 +1,38 @@
-myDate = new Date();
-myDate = myDate.getDay();
+/*
+Rotate by Day of Week: Use HTML/CSS & JavaScript to create a single page that rotates unique colors and content for each weekday (Sunday to Saturday) into the page.  The content must include:
 
-switch(myDate){
+	One unique image, with appropriate and matching content in the alt tag.  
+	A paragraph or two of content that describes the daily item (paragraph must include the name of the highlighted weekday)
+	A unique color that supports the image and paragraph of content
+
+The unique color could affect the background HTML, or an element on the page for each day of the week.  All of the above must occur within one page.
+name - for eample bubble tea
+pic 0 fir exanoke job
+akt - name of the art
+color - color of associcated with that coffie
+desc - desription of coffee
+day - day of he week
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+let myDate = new Date();
+let myDay = myDate.getDay();
+let coffee = "";
+let today = "";
+
+switch(myDay){
 
 	case 0:
     	today = "Sunday";
@@ -11,12 +42,37 @@ switch(myDate){
     	today = "Tuesday";
 	break;
 
+	case 3:
+    	today = "Wednesday";
+
+		coffee = {
+			name: "Bubble Tea",
+			pic: "images/bubble-tea.jpg",
+			alt: "A pic of a bubble tea",
+			color: "pink",
+			day: "Wednesday",
+			desc: `I like me some bubbble tea`
+			
+		};
+	break;
 
 
 	default:
-  	today = "Something went wrong"
+		today = "Something went Wrong"
+
+
 
 
 }
 
 alert(today);
+console.log(coffee);
+
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+
+
+function coffeeTemplate(coffee){
+	return `<p>
+         	<img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+        	<strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>,${coffee.desc}</p>`;
+}
