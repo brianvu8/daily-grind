2b30e687d8e27a3fd5d54f38d3d9d06f17139754
Brian Vu <brianvu888@gmail.com>
2024-11-26 13:22:54 -0800

    done

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,37 +1,8 @@
-/*
-Rotate by Day of Week: Use HTML/CSS & JavaScript to create a single page that rotates unique colors and content for each weekday (Sunday to Saturday) into the page.  The content must include:
-
-	One unique image, with appropriate and matching content in the alt tag.  
-	A paragraph or two of content that describes the daily item (paragraph must include the name of the highlighted weekday)
-	A unique color that supports the image and paragraph of content
-
-The unique color could affect the background HTML, or an element on the page for each day of the week.  All of the above must occur within one page.
-name - for eample bubble tea
-pic 0 fir exanoke job
-akt - name of the art
-color - color of associcated with that coffie
-desc - desription of coffee
-day - day of he week
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
 let myDate = new Date();
 let myDay = myDate.getDay();
-let coffee = "";
+//declare variable as temp value to change later on.
 let today = "";
-
+let coffee = "";
 
 //use location object to access querystring (address bar)
 const queryString = window.location.search;
@@ -42,72 +13,147 @@ console.log(queryString);
 //separate query string parameters
 const urlParams = new URLSearchParams(queryString);
 
+
 if(urlParams.has("day")){//from querystring
-	myDay = urlParams.get("day");
- }
- 
- myDay = parseInt(myDay);//must change to integer for switch
-
-
-//myDay = 3;//TEST ONLY
-
-switch(myDay){
-
-	case 0:
-    	today = "Sunday";
-	break;
-
-	case 1:
-    	today = "Monday";
-
-		coffee = {
-			name: "Cold Brew",
-			pic: "images/cold-brew.jpg",
-			alt: "A pic of a cold brew",
-			color: "brown",
-			day: "Monday",
-			desc: `I like me some cold brew`
-			
-		};
-	break;
-
-   case 2:
-    	today = "Tuesday";
-	break;
-
-	case 3:
-    	today = "Wednesday";
-
-		coffee = {
-			name: "Bubble Tea",
-			pic: "images/bubble-tea.jpg",
-			alt: "A pic of a bubble tea",
-			color: "pink",
-			day: "Wednesday",
-			desc: `I like me some bubbble tea`
-			
-		};
-	break;
-
-
-	default:
-		today = "Something went Wrong"
-
-
-
-
+    myDay = urlParams.get("day");
 }
 
+myDay = parseInt(myDay);
+
+
+switch(myDay){
+//theDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+//for(int i = 0; theDays.length() - 1)
+
+/*
+dialy-grind.js
+
+Rotate by Day of Week: Use HTML/CSS & JavaScript to create a single page that rotates unique colors and content for each weekday (Sunday to Saturday) into the page.  The content must include:
+One unique image, with appropriate and matching content in the alt tag.  
+A paragraph or two of content that describes the daily item (paragraph must include the name of the highlighted weekday)
+A unique color that supports the image and paragraph of content
+The unique color could affect the background HTML, or an element on the page for each day of the week.  All of the above must occur within one page.
+
+Here are the items we need for each coffee:
+name - Bubble Tea for example
+pic - an image of a bubble tea
+day - for example Wednesday
+alt - The data in the alt tag
+color - A color to match the coffee
+desc - A description of the coffee
+
+*/
+    case 0:
+        today = "Sunday";
+
+        coffee = {
+            name: "Cold-Brew",
+            pic: "images/cold-brew.jpg",
+            day: "Sunday",
+            alt: "A pic of a Cold-Brew",
+            color: "brown",
+            desc: `I love me a Cold-Brew!`
+        };
+        
+    break;
+
+    case 1:
+        today = "Monday";
+
+        coffee = {
+            name: "Pumpkin-Spice-Latte",
+            pic: "images/pumpkin-spice-latte.jpg",
+            day: "Monday",
+            alt: "A pic of a Pumpkin-Spice-Latte",
+            color: "pink",
+            desc: `Pumpkin-Spice-Latte's are perfect during these times!`
+        };
+    break;
+
+    case 2:
+        today = "Tuesday";
+
+        coffee = {
+            name: "Caramel-Latte",
+            pic: "images/caramel-latte.jpg",
+            day: "Tuesday",
+            alt: "A pic of a Caramel-Latte",
+            color: "purple",
+            desc: `Caramel-Latte's are the best ever!`
+        };
+
+    break;
+
+    case 3:
+        today = "Wednesday";
+
+        coffee = {            
+            name: "Bubble Tea",
+            pic: "images/bubble-tea.jpg",
+            day: "Wednesday",
+            alt: "A pic of a Bubble Tea",
+            color: "orange",
+            desc: `I like me some Bubble Tea!`
+        };
+
+    break;
+
+    case 4:
+        today = "Thursday";
+
+        coffee = {
+            name: "Frappaccino",
+            pic: "images/frappaccino.jpg",
+            day: "Thursday",
+            alt: "A pic of a Frappaccino",
+            color: "yellow",
+            desc: `Frappaccino's are so delicious!`
+        };
+
+    break;
+
+    case 5:
+        today = "Friday";
+
+        coffee = {
+            name: "Mocha",
+            pic: "images/mocha.jpg",
+            day: "Friday",
+            alt: "A pic of a Mocha",
+            color: "green",
+            desc: `Mocha is the best drink ever!`
+        };
+
+    break;
+
+    case 6:
+        today = "Saturday";
+
+        coffee = {
+            name: "Drip",
+            pic: "images/drip.jpg",
+            day: "Saturday",
+            alt: "A pic of a Drip",
+            color: "blue",
+            desc: `A classic drink calls for a Drip!`
+        };
+
+    break;
+
+    default:
+        today = "Something went wrong";
+
+}
 //alert(today);
 console.log(coffee);
-
 document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
-
 document.querySelector("html").style.backgroundColor = coffee.color;
 
-
 function coffeeTemplate(coffee){
-	return `<p>
-         	<img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
-        	<strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>,${coffee.desc}</p>`;
+    let myReturn = `<img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+        <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>`;
+
+    return(myReturn);
+    
 }
